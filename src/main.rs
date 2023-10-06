8ab67f91d14b6c3345e4d67371df392a323c6bd4
ofrighil <eugene.ofriel@gmail.com>
2023-10-05 20:48:31 -0400

    add argument paraser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::{error::Error, io::BufRead};
+
+use clap::{command, Command, arg};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+fn parse_args() {
+    // let matches = command!()
+    let matches = Command::new("ugit")
+        .subcommand_required(true)
+        .subcommand(
+            Command::new("init")
+                .about("Initialize new git repository")
+                .arg(arg!([NAME]))
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("init", sub_matches)) => {
+            println!("Hello World");
+        },
+        _ => unreachable!("No subcommand"), 
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    parse_args();
 }
