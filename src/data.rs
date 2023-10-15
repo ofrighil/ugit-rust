3e1e9f0c62e28c0a6da95b9218ad4b6f8ff7a753
ofrighil <eugene.ofriel@gmail.com>
2023-10-15 16:08:15 -0400

    get everything committed

--- src/data.rs
+++ src/data.rs
@@ -3,11 +3,5 @@ use std::{fs, env};
 pub const GIT_DIR: &str = ".ugit";
 
 pub fn init() -> std::io::Result<()> {
-    fs::create_dir(GIT_DIR)?;
-    println!(
-        "Initialized empty ugit repository in {}/{}",
-        env::current_dir().unwrap().to_str().unwrap(),
-        GIT_DIR
-    );
-    Ok(())
+    fs::create_dir_all(format!("{}/objects", GIT_DIR))
 }
--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,33 @@ fn parse_args() -> Result<(), std::io::Error> {
                 .about("Initialize new git repository")
                 .arg(arg!([NAME]))
         )
+        .subcommand(
+            Command::new("hash-object")
+                .about("Get the hash of a file object") 
+                .arg(arg!([NAME]))
+                .arg(arg!([NAME]))
+        )
         .get_matches();
 
     match matches.subcommand() {
-        Some(("init", _)) => {
-            data::init()
-        },
+        Some(("init", _)) => init(),
+        Some(("hash-object", sub_matches)) => hash_object(), 
         _ => unreachable!("No subcommand"), 
     }
+}
+
+fn init() -> Result<(), std::io::Error> {
+    data::init()?;
+    println!(
+        "Initialized empty ugit repository in {}/{}",
+        std::env::current_dir().unwrap().to_str().unwrap(),
+        data::GIT_DIR
+    );
+    Ok(())
+}
+
+fn hash_object(file) {
+
 }
 
 fn main() {
