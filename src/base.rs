d82242269006ff38806614d1c1948017805feb89
ofrighil <eugene.ofriel@gmail.com>
2023-10-27 20:16:58 -0400

    point 10: write-tree: Hash the files

--- src/base.rs
+++ src/base.rs
@@ -1,5 +1,7 @@
 use std::{fs, path::PathBuf};
 
+use crate::data;
+
 pub fn write_tree(directory: &str) {
     let dir = fs::read_dir(directory).unwrap();
 
@@ -10,9 +12,19 @@ pub fn write_tree(directory: &str) {
             continue
         }
 
-        if path.is_file() {
+        if path.is_symlink() {
+            continue
+        } else if path.is_file() {
             // todo!();
-            println!("{}", path.to_str().unwrap());
+            let path_str: &str = path.to_str().unwrap();
+            println!(
+                "{}, {}",
+                data::hash_object(
+                    std::fs::read(path_str).unwrap(),
+                    data::ObjectType::Blob
+                ).unwrap(),
+                path_str
+            );
         } else if path.is_dir() {
             write_tree(path.as_os_str().to_str().unwrap());
         }
