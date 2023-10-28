679601647503c3b230d23474264dfd8717752fcc
ofrighil <eugene.ofriel@gmail.com>
2023-10-27 20:03:48 -0400

    point 09: write-tree: Ignore .ugit files

--- src/base.rs
+++ src/base.rs
@@ -1,10 +1,15 @@
-use std::fs;
+use std::{fs, path::PathBuf};
 
 pub fn write_tree(directory: &str) {
     let dir = fs::read_dir(directory).unwrap();
 
     for entry in dir {
         let path = entry.unwrap().path();
+        
+        if is_ignored(&path) {
+            continue
+        }
+
         if path.is_file() {
             // todo!();
             println!("{}", path.to_str().unwrap());
@@ -12,4 +17,8 @@ pub fn write_tree(directory: &str) {
             write_tree(path.as_os_str().to_str().unwrap());
         }
     }
+}
+
+fn is_ignored(path: &PathBuf) -> bool {
+    path.to_str().unwrap().contains(".ugit")
 }
